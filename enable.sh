ef57c8354c81c65a1baba6ac7ee041ae7844a997
Allan Rui <arui@tenstorrent.com>
2022-03-09 21:58:42 +0000

    Define root in enable.sh script

--- enable.sh
+++ enable.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e # exit if a command fails
 
+if [ -n "$CI_PROJECT_DIR" ]; then
+  if [ -z $ROOT ]; then export ROOT=$CI_PROJECT_DIR; fi
+else
+  if [ -z $ROOT ]; then export ROOT=`git rev-parse --show-toplevel`; fi
+fi
+
 echo $ROOT
 export TVM_HOME=$ROOT/third_party/tvm
 export PYTHONPATH=$TVM_HOME/python:${PYTHONPATH}
