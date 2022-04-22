743f835858715b553aefad9461d963dd67d08354
Aleks Knezevic <aknezevic@tenstorrent.com>
2022-04-22 13:11:59 +0000

    Don't use ROOT env as its used by budabackend

--- enable.sh
+++ enable.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 if [ -n "$CI_PROJECT_DIR" ]; then
-  if [ -z $ROOT ]; then export ROOT=$CI_PROJECT_DIR; fi
+  if [ -z $PYBUDA_ROOT ]; then export PYBUDA_ROOT=$CI_PROJECT_DIR; fi
 else
-  if [ -z $ROOT ]; then export ROOT=`git rev-parse --show-toplevel`; fi
+  if [ -z $PYBUDA_ROOT ]; then export PYBUDA_ROOT=`git rev-parse --show-toplevel`; fi
 fi
 
-echo $ROOT
-export TVM_HOME=$ROOT/third_party/tvm
+echo $PYBUDA_ROOT
+export TVM_HOME=$PYBUDA_ROOT/third_party/tvm
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TVM_HOME/build
 pip install -e third_party/tvm/python
 
-cd $ROOT
+cd $PYBUDA_ROOT
--- install.sh
+++ install.sh
@@ -2,24 +2,22 @@
 set -e # exit if a command fails
 
 if [ -n "$CI_PROJECT_DIR" ]; then
-  if [ -z $ROOT ]; then export ROOT=$CI_PROJECT_DIR; fi
+  if [ -z $PYBUDA_ROOT ]; then export PYBUDA_ROOT=$CI_PROJECT_DIR; fi
 else
-  if [ -z $ROOT ]; then export ROOT=`git rev-parse --show-toplevel`; fi
+  if [ -z $PYBUDA_ROOT ]; then export PYBUDA_ROOT=`git rev-parse --show-toplevel`; fi
 fi
 
-echo $ROOT
-cd $ROOT
-make python_env
-source build/python_env/bin/activate
-export TVM_HOME=$ROOT/third_party/tvm
+echo $PYBUDA_ROOT
+cd $PYBUDA_ROOT
+export TVM_HOME=$PYBUDA_ROOT/third_party/tvm
 
 # Download / untar LLVM
-cd $ROOT/third_party
-if [ ! -d "$ROOT/third_party/llvm" ]; then
+cd $PYBUDA_ROOT/third_party
+if [ ! -d "$PYBUDA_ROOT/third_party/llvm" ]; then
   wget -nc https://github.com/llvm/llvm-project/releases/download/llvmorg-13.0.0/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-  LLVM_TAR=$ROOT/third_party/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-  LLVM_DIR=$ROOT/third_party/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04
-  tar -xf $LLVM_TAR && mv $LLVM_DIR $ROOT/third_party/llvm && rm -f $LLVM_TAR
+  LLVM_TAR=$PYBUDA_ROOT/third_party/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+  LLVM_DIR=$PYBUDA_ROOT/third_party/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04
+  tar -xf $LLVM_TAR && mv $LLVM_DIR $PYBUDA_ROOT/third_party/llvm && rm -f $LLVM_TAR
 fi
 
 cd $TVM_HOME
@@ -30,13 +28,13 @@ cp $TVM_HOME/cmake/config.cmake $TVM_HOME/build
 cd $TVM_HOME/build
 
 # Link the LLVM thats just been downloaded
-LLVM_LINK=$ROOT/third_party/llvm/bin/llvm-config
+LLVM_LINK=$PYBUDA_ROOT/third_party/llvm/bin/llvm-config
 sed -i "s#/usr/bin/llvm-config#$LLVM_LINK#g" $TVM_HOME/build/config.cmake
 
-if [ "$CONFIG" == "ci" ] || [ "$CONFIG" == "" ] || [ "$CONFIG" == "release" ]; then
-  export TVM_BUILD_CONFIG="Release"
-else
+if [ "$CONFIG" == "debug" ]; then
   export TVM_BUILD_CONFIG="Debug"
+else
+  export TVM_BUILD_CONFIG="Release"
 fi
 
 echo $TVM_BUILD_CONFIG
@@ -44,4 +42,4 @@ cmake -j$(nproc) -DCMAKE_BUILD_TYPE=$TVM_BUILD_CONFIG $TVM_HOME
 make -j$(nproc)
 
 echo "TVM Built Successful"
-cd $ROOT
+cd $PYBUDA_ROOT
