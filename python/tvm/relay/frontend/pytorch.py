489927361ea5107d0b1501ff624149fd87feba99
Lewis Panos <lpanos@tenstorrent.com>
2023-04-20 22:07:44 +0000

    Add diagonal for triu decomp

--- python/tvm/relay/frontend/pytorch.py
+++ python/tvm/relay/frontend/pytorch.py
@@ -4298,7 +4298,7 @@ class PyTorchOpConverter:
         x = inputs[0]
         x_shape = _infer_shape(x)
 
-        mask = np.triu(np.ones(x_shape)).astype(np.bool)
+        mask = np.triu(np.ones(x_shape), inputs[1]).astype(np.bool)
         mask = tvm.nd.array(mask)
         mask = tvm.relay.Constant(mask)
 
