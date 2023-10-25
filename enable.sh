76ab7726921044eff200cea0dbaea9706ac88c1f
Nicholas Smith <nsmith@tenstorrent.com>
2023-10-25 15:43:57 -0500

    Move pip install from enable.sh to install.sh

--- enable.sh
+++ enable.sh
@@ -6,9 +6,7 @@ else
   if [ -z $PYBUDA_ROOT ]; then export PYBUDA_ROOT=`git rev-parse --show-toplevel`; fi
 fi
 
-echo $PYBUDA_ROOT
 export TVM_HOME=$PYBUDA_ROOT/third_party/tvm
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TVM_HOME/build
-pip install -e third_party/tvm/python
 
 cd $PYBUDA_ROOT
