db83a65fbb753eae6abbc468658c01d6c8090cb5
jserbedzija <jserbedzija@tenstorrent.com>
2024-08-15 10:05:54 +0000

    Remove flax dependency from TVM

--- python/tvm/contrib/forge_compile.py
+++ python/tvm/contrib/forge_compile.py
@@ -40,8 +40,7 @@ from jax.experimental import jax2tf
 from jax.tools.jax_to_ir import tf_wrap_with_input_names
 import collections
 from transformers.utils.generic import ModelOutput
-from transformers.modeling_flax_utils import FlaxPreTrainedModel
-from tvm.contrib.forge_utils import (
+from tvm.contrib.pybuda_utils import (
     extract_framework_model_outputs, 
     extract_flatten_inputs, 
     construct_tvm_ir,
@@ -753,12 +752,16 @@ def compile_jax_for_forge(jaxmodel, *inputs, graph_name, compiler_cfg, verify_cf
 
         return dict(items)
 
-    if isinstance(jaxmodel, FlaxPreTrainedModel):
-        model_params = jaxmodel.params
-    else:
-        model_params = {}
-        if hasattr(jaxmodel, 'params'):
-            model_params = jaxmodel.variables['params']._dict
+    # if isinstance(jaxmodel, FlaxPreTrainedModel):
+    #     model_params = jaxmodel.params
+    # else:
+    #     model_params = {}
+    #     if hasattr(jaxmodel, 'params'):
+    #         model_params = jaxmodel.variables['params']._dict
+
+    model_params = {}
+    if hasattr(jaxmodel, 'params'):
+        model_params = jaxmodel.variables['params']._dict
     
     weight_names = list(flatten_params(model_params).keys())
     json_graphs = extract_graphs(partitioned_mod, forge_params, flattened_input_names,weight_names, param_name_lookup, graph_hash=m.hexdigest())
@@ -1009,12 +1012,17 @@ def format_tvm_graph_weights(inputs, module, compiler_cfg, framework=None):
 
             return dict(items)
 
-        if isinstance(module, FlaxPreTrainedModel):
-            module_params = module.params
-        else:
-            module_params = {}
-            if hasattr(module, 'params'):
-                module_params = module.variables['params']._dict
+        # if isinstance(module, FlaxPreTrainedModel):
+        #     module_params = module.params
+        # else:
+        #     module_params = {}
+        #     if hasattr(module, 'params'):
+        #         module_params = module.variables['params']._dict
+
+        module_params = {}
+        if hasattr(module, 'params'):
+            module_params = module.variables['params']._dict
+            
         module_params = flatten_params(module_params)
 
         weights = {}
