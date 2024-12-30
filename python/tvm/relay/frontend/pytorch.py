fc69dc2d5d515109fd48dadf26be8ee13864b302
Ashok Kumar Kannan <akannan@tenstorrent.com>
2024-12-30 08:29:32 +0000

    Map aten bitwise_or_

--- python/tvm/relay/frontend/pytorch.py
+++ python/tvm/relay/frontend/pytorch.py
@@ -4893,6 +4893,7 @@ class PyTorchOpConverter:
             "aten::mv": self.mv,
             "aten::grid_sampler": self.grid_sampler,
             "aten::__ior__": self.make_elemwise("bitwise_or"),
+            "aten::bitwise_or_": self.make_elemwise("bitwise_or"),
             "aten::__iand__": self.make_elemwise("bitwise_and"),
             "aten::__ixor__": self.make_elemwise("bitwise_xor"),
             "aten::__lshift__": self.make_elemwise("left_shift"),
